d3bfab1ba8e58278e6ba3315356f2d4ff131a9af
Laura <laurabande10@gmail.com>
2021-01-25 14:11:55 +0100

    Corrección errores

--- src/main/java/ej25/Cancion.java
+++ src/main/java/ej25/Cancion.java
@@ -5,26 +5,73 @@
  */
 package ej25;
 
+import java.util.Objects;
+
 /**
  *
  * @author laura
  */
 public class Cancion {
-    private static String nombre, artista, album;
-    private static EstiloMusical estilo;
+    private String nombre, artista, album;
+    private EstiloMusical estilo;
     
-	public static String getNombre() {
+	public String getNombre() {
 		return nombre;
 	}
-	public static String getArtista() {
+	public String getArtista() {
 		return artista;
 	}
-	public static String getAlbum() {
+	public String getAlbum() {
 		return album;
 	}
-	public static EstiloMusical getEstilo() {
+	public EstiloMusical getEstilo() {
 		return estilo;
 	}
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    /* EQUALS
+        Necesito este metodo para usar el metodo remove el, 
+        metodo contains y el indexof
+    */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cancion other = (Cancion) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.artista, other.artista)) {
+            return false;
+        }
+        if (!Objects.equals(this.album, other.album)) {
+            return false;
+        }
+        if (this.estilo != other.estilo) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cancion{" + "nombre=" + nombre + ", artista=" + artista + ", album=" + album + ", estilo=" + estilo + '}';
+    }
+
     
+    
+        
     
 }
--- src/main/java/ej25/ListaReproduccion.java
+++ src/main/java/ej25/ListaReproduccion.java
@@ -13,60 +13,76 @@ import java.util.Collections;
  * @author laura
  */
 public class ListaReproduccion {
+
     private ArrayList<Cancion> lista;
     private int contadorCanciones = 0;
 
-	public ListaReproduccion() {
-		this.lista = new ArrayList<>();
-		//Falta inicializar
-		//lista.add();
-		contadorCanciones++; //Hacer tras cada canción incluida
-	}
-	
-	public int numeroCanciones() {
-		return contadorCanciones;
-	}
-	
-	public boolean estaVacia() {
-		return (contadorCanciones == 0);
-	}
-	
-	public Cancion escucharCancion(int pos) {
-		return this.lista.get(pos);
-	}
-	
-	public void cambniarCancion(int pos, Cancion song) {
-		this.lista.set(pos, song);
-	}
-	
-	public void grabarCancion(Cancion song) {
-		this.lista.add(song);
-	}
-	
-	public void eliminaCancion(int pos) {
-		this.lista.remove(pos);
-	}
+    public ListaReproduccion() {
+        this.lista = new ArrayList<>();
+    }
+
+    public int numeroCanciones() {
+        return contadorCanciones;
+    }
+
+    public boolean estaVacia() {
+        return lista.isEmpty();
+    }
+
+    public Cancion escucharCancion(int pos) {
+        if (pos >= 0 && pos < lista.size()) {
+            return this.lista.get(pos);
+        }
+        return null;
+    }
+
+    public void cambniarCancion(int pos, Cancion song) {
+        if (pos >= 0 && pos < lista.size()) {
+            this.lista.set(pos, song);
+        }
+    }
+
+    public void grabarCancion(Cancion song) {
+        this.lista.add(song);
+    }
+
+    public void eliminaCancion(int pos) {
+        //Mirar que la posición es correcta
+        if (pos >= 0 && pos < lista.size()) {
+            this.lista.remove(pos);
+        }
+    }
     
+    /*
+    SI LA CLASE CANCIÓN NO TIENE IMPLEMANTADO EL METODO EQUALS
+    EL METODO REMOVE NO SE GARANTIZA QUE FUNCIONE CORRECTAMENTE
+    */
+    public void eliminaCancion(Cancion cancion) {
+        //Mirar que la posición es correcta
+        this.lista.remove(cancion);
+    }
+
     public static void imprimirLista(ListaReproduccion tmp) {
-    	for(int i = 0; i < tmp.lista.size(); i++) {
-    		System.out.println(tmp.lista.get(i).getNombre());
-    	}
+        for (int i = 0; i < tmp.lista.size(); i++) {
+            System.out.println(tmp.lista.get(i).getNombre());
+        }
     }
-    
-    public int buscarCancion (Cancion c) {
-    	return lista.indexOf(c);
+
+    //SI NO TENGO EL MÉTODO EQUALS NO FUNCIONA
+    public int buscarCancion(Cancion c) {
+        return lista.indexOf(c);
     }
-    
+
     public void ordenarTitulo() {
-    	//Collections.sort(this.lista, Cancion.getNombre());
+        //Collections.sort(this.lista, Cancion.getNombre());
     }
-    
+
     public void ordenarArtista() {
-    	//Collections.sort(Cancion.getArtista());
+        //Collections.sort(Cancion.getArtista());
     }
-    
+
     public void buscar() {
-    	//Collections.binarySearch(lista, Cancion.getNombre());
+        //Collections.binarySearch(lista, Cancion.getNombre());
     }
-    
+
 }
